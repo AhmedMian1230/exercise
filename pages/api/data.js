e9956ae855b3def33f998edb7bc238f9238f7f91
Ahmed Mian <asmian1230@gmail.com>
2021-04-17 08:58:31 -0400

    Implemented /api/data and /api/savings endpoints

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,5 +1,25 @@
-// TODO: Create /api/data REST endpoint
+import axios from 'axios'
 
-export default (req, res) => {
-  res.status(200).json({ TODO: '/api/data endpoint' })
+export default async (req, res) => {
+  const defenseDataUrl = 'https://www.defense.gov/data.json'
+  let httpCode = 500
+  let response = {
+    error: `Failed to fetch data from ${defenseDataUrl}`
+  }
+  if (req.method !== 'GET') {
+    httpCode = 400
+    response.error = 'The /api/data endpoint only accepts GET requests.'
+  } else {
+    try {
+      const defenseData = await axios.get(defenseDataUrl)
+      if (defenseData.data) {
+        httpCode = 200
+        response = defenseData.data
+      }
+    } catch (err) {
+      console.log(`Error fetching data from ${defenseDataUrl}. `, err)
+    }
+  }
+  console.log(`Called ${req.method} on /api/data.`)
+  res.status(httpCode).json(response)
 }
--- pages/api/savings.js
+++ pages/api/savings.js
@@ -1,6 +1,25 @@
-// TODO: Create /api/savings REST endpoint
+import axios from 'axios'
 
-export default (req, res) => {
-    res.status(200).json({ TODO: '/api/savings endpoint' })
+export default async (req, res) => {
+  const costSavingsUrl = 'https://www.defense.gov/digitalstrategy/costsavings.json'
+  let httpCode = 500
+  let response = {
+    error: `Failed to fetch data from ${costSavingsUrl}`
   }
-  
+  if (req.method !== 'GET') {
+    httpCode = 400
+    response.error = 'The /api/savings endpoint only accepts GET requests.'
+  } else {
+    try {
+      const savingsData = await axios.get(costSavingsUrl)
+      if (savingsData.data) {
+        httpCode = 200
+        response = savingsData.data
+      }
+    } catch (err) {
+      console.log(`Error fetching data from ${costSavingsUrl}. `, err)
+    }
+  }
+  console.log(`Called ${req.method} on /api/savings.`)
+  res.status(httpCode).json(response)
+}
